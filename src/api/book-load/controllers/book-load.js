4f72930cefc611779ca84c2ba01ef8be684ceff3
asurasdevaas <ewarams@gmail.com>
2025-04-19 19:41:07 -0500

    add plugin railway

---
+++ src/api/book-load/controllers/book-load.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * book-load controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::book-load.book-load');
---
+++ src/api/book-load/routes/book-load.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * book-load router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::book-load.book-load');
